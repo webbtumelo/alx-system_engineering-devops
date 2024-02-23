e8fd9a6b5ab3f26c35c9d098a356f988d674eec2
webbtumelo <webbtumelo@gmail.com>
2024-02-24 01:28:33 +0200

    update-task2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""Returns Todo list for a given employee id
-and axports data in the CSV format"""
+# Using what you did in the task #0, extend
+# your Python script to export data in the CSV format.
+
 import requests
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    id = argv[1]
-
-    user = requests.get(url + f"users/{id}").json()
-    todos = requests.get(url + f"todos", params={"userId": id}).json()
-
-    file = f"{id}.csv"
-    with open(file, "w") as f:
-        for item in todos:
-            f.write(
-                f'"{id}","{user["username"]}","{item["completed"]}", "{item["title"]}"\n'
-            )
+    USER_ID = sys.argv[1]
+    jsonplaceholder = 'https://jsonplaceholder.typicode.com/users'
+    url = jsonplaceholder + '/' + USER_ID
+    response = requests.get(url)
+    username = response.json().get('username')
+    todo_url = url + '/todos'
+    response = requests.get(todo_url)
+    tasks = response.json()
+    with open(USER_ID + '.csv', 'w') as f:
+        for task in tasks:
+            f.write('"{}","{}","{}","{}"\n'.format(USER_ID, username,
+                                                task.get('completed'),
+                                                task.get('title')))
